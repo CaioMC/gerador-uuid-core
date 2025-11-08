2165b1e812ea9972c7a8fb9fca01d2bafde77c61
caio.coelho <caio.coelho@totvs.com.br>
2025-11-07 21:49:33 -0300

    merge

---
+++ src/main/java/com/fiap/pj/core/GeradorUuidCoreApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.pj.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorUuidCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorUuidCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/pj/core/GeradorUuidCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.pj.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorUuidCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
